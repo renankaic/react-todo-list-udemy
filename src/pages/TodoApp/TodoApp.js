797c5e9593538d095d4a44181f768eb7af2bc828
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-03-18 13:57:28 -0300

    Criando página das tarefas

---
+++ src/pages/TodoApp/TodoApp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function TodoApp() {
+
+    return (
+        <>
+            <header>TodoCreator</header>
+            <main>TodoList</main>
+            <footer>TodoFilter</footer>
+        </>
+    )
+
+}
+
+export default TodoApp
--- src/state/filter/reducer.js
+++ src/state/filter/reducer.js
@@ -14,3 +14,5 @@ function reducer(state, action) {
     }
 
 }
+
+export default reducer
