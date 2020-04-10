f1c788cc53a8893e3f69b808272350ebcea395cc
Renan Kaic Lopes <renan-kaic@outlook.com>
2020-04-10 13:59:18 -0300

    Feat - Adding options and actions to TodoSelect container

--- src/pages/TodoApp/containers/TodoFilter/TodoFilter.js
+++ src/pages/TodoApp/containers/TodoFilter/TodoFilter.js
@@ -1,21 +1,40 @@
-import React, { useContext, useCallback } from 'react'
+import React, { useContext, useCallback, useState, useEffect } from 'react'
 import FilterContext from '../../../../state/filter/Context'
 import * as filterActions from '../../../../state/filter/actions'
+import TodoSelect from './TodoSelect/TodoSelect'
 import styles from './TodoFilter.module.css'
 
 function TodoFilter() {
 
     const { filter, dispatchToFilter } = useContext(FilterContext)
 
-    const handleFilterUpdate = useCallback((filter) => {
+    const [selectValue, setSelectValue] = useState(filter)
+
+    const updateFilter = useCallback((filter) => {
         dispatchToFilter(filterActions.toggleFilter(filter))
     }, [dispatchToFilter])
+
+    const handleOptionChange = useCallback((evt) => {
+        setSelectValue(evt.target.value)
+    }, [setSelectValue])
+
+    useEffect(() => {
+        updateFilter(selectValue)
+    }, [updateFilter, selectValue])
 
     return(
         <footer>
             <div className={styles.container}>
                 <h1>{filter}</h1>
-                <button onClick={() => { handleFilterUpdate('active') }}>Alterar filtro</button>
+                <TodoSelect 
+                    value={selectValue} 
+                    onOptionChange={handleOptionChange} 
+                    options={[
+                        { value: 'all', title: 'Todas as tarefas' },
+                        { value: 'active', title: 'Tarefas a se fazer' },
+                        { value: 'completed',  title: 'Tarefas realizadas' }
+                    ]}
+                />
             </div>
         </footer>
     )
